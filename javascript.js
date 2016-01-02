4380dfe3a3bd0b670a1dda65abfb3edf1bfc7b24
Jakub Pas <jakubpas@gmail.com>
2016-01-02 14:08:21 +0100

    allow to use keyboard on remote

--- javascript.js
+++ javascript.js
@@ -1,39 +1,52 @@
-$(function(){
-    $('.key').on('click',function(){
-        makeRequest('samsungremote.php', '?key=' + $(this).data('key'));
+$(function () {
+    $('.key').on('click', function () {
+        makeRequest($(this).data('key'));
     });
-    var http_request = false;
 
-    function makeRequest(url, parameters) {
+    $(document).keydown(function (e) {
+        switch (e.which) {
+            case 37:
+                makeRequest('LEFT');
+                break;
+            case 38:
+                makeRequest('UP');
+                break;
+            case 39:
+                makeRequest('RIGHT');
+                break;
+            case 40:
+                makeRequest('DOWN');
+                break;
+            case 13:
+                makeRequest('ENTER');
+                break;
+            default:
+                return;
+        }
+        e.preventDefault();
+    });
+
+    function makeRequest(parameters) {
+        var url = 'samsungremote.php';
+        parameters = '?key=' + parameters;
         if (window.XMLHttpRequest) {
             http_request = new XMLHttpRequest();
             if (http_request.overrideMimeType) {
                 http_request.overrideMimeType('text/html');
-            }
-        } else if (window.ActiveXObject) {
-            try {
-                http_request = new ActiveXObject("Msxml2.XMLHTTP");
-            } catch (e) {
-                try {
-                    http_request = new ActiveXObject("Microsoft.XMLHTTP");
-                } catch (e) {
-                }
             }
         }
         if (!http_request) {
             alert('Cannot create XMLHTTP instance');
             return false;
         }
-        http_request.onreadystatechange = alertContents;
+        http_request.onreadystatechange = alertContents(http_request);
         http_request.open('GET', url + parameters, true);
         http_request.send(null);
     }
 
-    function alertContents() {
+    function alertContents(http_request) {
         if (http_request.readyState == 4) {
-            if (http_request.status == 200) {
-                alert(http_request.responseText);
-            } else {
+            if (http_request.status != 200) {
                 alert('There was a problem with the request.');
             }
         }
